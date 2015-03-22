1c900f5697f2b036e90265707c70065775d6ad75
Jefffrey <notjefff@gmail.com>
2015-03-22 20:27:14 +0100

    Added check that a word is actually a word

--- src/Main.hs
+++ src/Main.hs
@@ -2,19 +2,35 @@ module Main where
 
 import Titim.Grid
 import Control.Monad (foldM_)
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+data Game = Game Grid (Set String) (Set String)
 
 main :: IO ()
 main = do
+    dictionary <- readFile "dictionary.txt"
     initial <- makeStep $ startGrid (10, 10)
     foldM_ 
-        (\grid _ -> do
+        (\(Game grid words usedWords) i -> do
             putStr "\o33c"
+            putStrLn $ "Round " ++ show i
             putStrLn "----------------------------"
             print grid
             putStrLn "----------------------------"
-            putStrLn "Your answer: "
+            putStrLn "Give me a word: "
             word <- getLine
-            grid' <- makeStep grid
-            return . hitWithWord word $ grid') 
-        initial
+            if word `Set.member` words
+                then do
+                    grid' <- makeStep $ hitWithWord word grid
+                    return $ Game grid' (Set.delete word words) (Set.insert word usedWords)
+                else do
+                    if word `Set.member` usedWords
+                        then putStrLn "You used that one already"
+                        else putStrLn "That's not a word you fool"
+                    putStrLn "Say something bad to me: "
+                    _ <- getLine
+                    grid' <- makeStep grid
+                    return $ Game grid' words usedWords)
+        (Game initial (Set.fromList $ lines dictionary) Set.empty)
         [1..]
